61ebf4c891c326a92cf8fecaa7a5e77ef46803ee
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-06-07 19:20:12 -0700

    📉 added utils file and imported some dependencies

---
+++ server.go
@@ -0,0 +1,10 @@
+// server.go
+
+package main
+
+// import (
+// // "github.com/gorilla/mux"
+// // "github.com/jinzhu/gorm"
+// // "github.com/dgrijalva/jwt-go"
+// // "github.com/joho/godotenv"
+// )
