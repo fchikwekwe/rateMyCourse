756c7e61a6529f2e9f8abfb1cecd8ddc70f1d81e
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-07-16 13:02:52 -0700

    👍🏾 simple server up and running

--- server.go
+++ server.go
@@ -2,9 +2,32 @@
 
 package main
 
-// import (
-// // "github.com/gorilla/mux"
-// // "github.com/jinzhu/gorm"
-// // "github.com/dgrijalva/jwt-go"
-// // "github.com/joho/godotenv"
-// )
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	// "github.com/labstack/echo/engine/standard"
+)
+
+func main() {
+	e := echo.New()
+
+	// // Middleware
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+
+	// // CORS
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	AllowOrigins: []string{"*"},
+	// 	AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	// }))
+
+	// // Server
+	// e.Run(standard.New(":1323"))
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
