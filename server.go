d2fa7b3ea95ea7de150de0bbfe8667ede0e4452e
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-08-05 12:31:32 -0700

    🔗 added some middleware to help with error messaging

--- server.go
+++ server.go
@@ -6,13 +6,24 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	// Echo instance
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start Server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Testing!")
 }
