bc6dbf9f0aa1c7b525b99da8cbce12628f641d06
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-08-05 12:23:45 -0700

    📄 removed some commented out code

--- server.go
+++ server.go
@@ -6,26 +6,11 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	// "github.com/labstack/echo/engine/standard"
 )
 
 func main() {
 	e := echo.New()
 
-	// // Middleware
-	// e.Use(middleware.Logger())
-	// e.Use(middleware.Recover())
-
-	// // CORS
-	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	// 	AllowOrigins: []string{"*"},
-	// 	AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-	// }))
-
-	// // Server
-	// e.Run(standard.New(":1323"))
-
-	// Route => handler
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!\n")
 	})
